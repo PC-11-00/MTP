632a75f42f74a743e5f50bfb6acfa337a2ed07e9
Pushpendra Kumar <b20122@students.iitmandi.ac.in>
2024-03-21 18:01:52 +0530

    Beautify the App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { UploadPdfComponent } from './upload-pdf/upload-pdf.component';
 import { PlagiarismResultComponent } from './plagiarism-result/plagiarism-result.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon'; // If you plan to use icons
+import { FormsModule } from '@angular/forms'; // For template-driven forms
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +21,13 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
